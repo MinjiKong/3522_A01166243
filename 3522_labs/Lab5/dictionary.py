5a332543d93208c920f975ea7f73f8c1e9e062f9
jkcadee <jkwok55@my.bcit.ca>
2021-02-11 00:27:13 -0800

    dict and file handler commented and finished

--- 3522_labs/Lab5/dictionary.py
+++ 3522_labs/Lab5/dictionary.py
@@ -1,5 +1,6 @@
-import json
+import difflib
 from file_handler import *
+
 
 class Dictionary:
 
@@ -12,8 +13,38 @@ class Dictionary:
         Loads data into a dictionary.
         :param filepath: The file path to load the data into.
         :precondition: Filepath must be a string and a filepath to an actual file
-        :return:
+        :return: None
         """
 
-        FileHandler.load_data(filepath, self._file_extension)
-        return
+        try:
+            self._data = FileHandler.load_data(filepath, self._file_extension)
+        except Exception as e:
+            print(e)
+            exit(-1)
+
+    def query_dictionary(self, word):
+        """
+        Queries the dictionary for a specific word. If the word is not found, nothing is returned
+        :param word: The word that we are querying for.
+        :precondition: Word must be a String.
+        :return: String
+        """
+
+        key_list = self._data.keys()
+        try:
+            found_key = difflib.get_close_matches(word.lower(), key_list, n=3, cutoff=0.6)
+            return self._data[found_key[0]]
+        except AttributeError as e:
+            print(e)
+        except IndexError as e:
+            print(e)
+
+
+def main():
+    dictionary = Dictionary('.json')
+    dictionary.load_dictionary("data.json")
+    print(dictionary.query_dictionary(input("Input the word you are looking for here: ")))
+
+
+if __name__ == "__main__":
+    main()
--- 3522_labs/Lab5/file_handler.py
+++ 3522_labs/Lab5/file_handler.py
@@ -4,25 +4,42 @@ import pathlib
 
 
 class FileExtensions(enum.Enum):
+    """
+    Enum for file extensions.
+    Can accept .txt and .json extensions.
+    """
     TXT = '.txt'
     JSON = '.json'
 
 
 class FileHandler:
+    """
+    File handler class that can load data from files or write lines to files.
+    """
 
     def __init__(self):
+        """
+        Constructor for FileHandler. Initializes a file_data variable.
+        """
         self._file_data = None
 
     @staticmethod
     def load_data(path, file_extension):
+        """
+        Loads the data from the file specified at the path.
+        :param path: File path.
+        :param file_extension: The type of extension.
+        :precondition: The path and file extension must exist and be valid. If not, then the program will exit.
+        :return: Returns the data in the file in a dictionary format. Dict.
+        """
         if pathlib.Path(path).is_file():
             try:
-                if file_extension == FileExtensions.TXT:
+                if file_extension == FileExtensions.TXT.value:
                     with open(path, mode="r", encoding="utf-8") as data_file:
                         data = data_file.read()
                         data_file.close()
                         return data
-                elif file_extension == FileExtensions.JSON:
+                elif file_extension == FileExtensions.JSON.value:
                     with open(path, mode="r", encoding="utf-8") as data_file:
                         data = json.load(data_file)
                         data_file.close()
@@ -31,17 +48,32 @@ class FileHandler:
                     raise InvalidFileTypeError
             except InvalidFileTypeError as e:
                 print(e)
+        else:
+            raise FileNotFoundError("File is not found!")
 
     @staticmethod
     def write_lines(path, lines):
-        try:
-            with open(path, "a") as data_file:
-                data_file.write(lines)
-                data_file.close()
-        except Exception as e:
-            print(e)
+        """
+        Writes lines into a txt file (will create it if it does not exist).
+        :param path: File path.
+        :param lines: The lines to be written onto the text file.
+        :precondition: The path must exist and be valid. If not, then the program will exit.
+        :return: None
+        """
+        if pathlib.Path(path).is_file():
+            try:
+                with open(path, "a") as data_file:
+                    data_file.write(lines)
+                    data_file.close()
+            except Exception as e:
+                print(e)
+            else:
+                raise FileNotFoundError("File is not found!")
 
 
 class InvalidFileTypeError(Exception):
+    """
+
+    """
     def __init__(self):
         super().__init__("File is not a TXT or JSON file!")
